4f7bc3a6d8a32a4a7621c09fc9dd5781bb699f92
Bloo00 <kabin.yangg@gmail.com>
2022-04-21 16:12:59 -0700

    added cool new platforms not really cool or new

--- index.js
+++ index.js
@@ -31,18 +31,18 @@ class Player {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
-        
         else this.velocity.y = 0
     }
 }
 // ============================================================================= platform
 class Platform {
-    constructor() {
+    constructor({x,y,width,height}) {
         this.position = {
-            x: 0,
-            y: 0
+            x,          // means x: x 
+            y           // ditto
         }
 
         this.width = 200
@@ -55,9 +55,11 @@ class Platform {
     }
 }
 
+// ========================================================================== Game objects
 
-const player = new Player()
-const platform = new Platform()
+const player = new Player() // player
+const platforms = [new Platform({x:200,y:200}
+), new Platform({x:500,y:200})] // platform
 
 
 // ============================================================================ controls
@@ -72,36 +74,70 @@ const keys = {
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
+    // ================== updating and drawing
+    platforms.forEach(platform => {
+        platform.draw()
+    })
     player.update()
-    // for left and right
-    if (keys.right.pressed) {
-        player.velocity.x       = 5
-    }else if (keys.left.pressed) {
-        player.velocity.x       = -5
-    }else player.velocity.x     = 0
 
+    // for left and right
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x                       = 5
+    }else if (keys.left.pressed&& player.position.x > 100) {
+        player.velocity.x                       = -5
+    }else { 
+        player.velocity.x                       = 0
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.draw()
+                platform.position.x                 -= 5
+            })
+        }
+        else if (keys.left.pressed){
+            platforms.forEach(platform => {
+                platform.draw()
+                platform.position.x                 += 5
+            })
+        }
+    }
+
+    // collision *** oh god this is ugly to see if you know how to make it better pls do tell
+    platforms.forEach(platform => {
+    if (
+        player.position.y + player.height                           <= platform.position.y 
+        && player.position.y + player.height + player.velocity.y    >= platform.position.y 
+        && player.position.x + player.width                         >= platform.position.x 
+        && player.position.x                                        <= platform.position.x +     platform.width) {
+        player.velocity.y = 0
+        }
+    })
 }
 
 animate()
 
 // ============================================================================= event listeners
 
-
-// todo gotta make a jump thingy
+/***
+ * todo make no double jump xxx
+ * todo make able jump free fall
+ *
+ */
 addEventListener('keydown', ({ keyCode }) => {
     switch ( keyCode ) {
         case 65:
             keys.left.pressed = true 
             break;
         case 83:
-            keys.down.pressed = true 
+            // keys.down.pressed = true 
             break;
         case 68:
             keys.right.pressed = true 
             break;
         case 87:
-
-            keys.up.pressed = true 
+            if (player.velocity.y == 0) {
+                player.velocity.y -= 20
+            }
+            // keys.up.pressed = true 
             break;
     }
 })
@@ -112,13 +148,13 @@ addEventListener('keyup', ({ keyCode }) => {
             keys.left.pressed = false 
             break;
         case 83:
-            keys.down.pressed = false 
+            // keys.down.pressed = false 
             break;
         case 68:
             keys.right.pressed = false 
             break;
         case 87:
-            keys.up.pressed = false 
+            // keys.up.pressed = false 
             break;
     }
 })
